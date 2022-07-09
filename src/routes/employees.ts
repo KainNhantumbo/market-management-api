feeecc9d94bd3feae1192d981e0fcee14a35b285
Kain Nhantumbo <nhantumbok@gmail.com>
2022-07-09 12:59:13 +0200

    feat: added employee routes.

--- src/routes/employees.ts
+++ src/routes/employees.ts
@@ -2,9 +2,17 @@ import { Router } from 'express';
 import EmployeesController from '../controllers/employees';
 
 const router = Router();
-
 const controller = new EmployeesController();
 
-router.route('/').get(controller.getAllEmployees).post(controller.createEmployee);
+router
+	.route('/')
+	.get(controller.getAllEmployees)
+	.post(controller.createEmployee);
+
+router
+	.route('/:id')
+	.get(controller.getEmployee)
+	.patch(controller.updateEmployee)
+	.delete(controller.deleteEmployee);
 
 export { router };
