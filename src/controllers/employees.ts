a5aa110bb0254f601368a646456e56c402c93e9b
Kain Nhantumbo <nhantumbok@gmail.com>
2022-07-09 09:42:15 +0200

    feat: added get employee controller.

--- src/controllers/employees.ts
+++ src/controllers/employees.ts
@@ -5,7 +5,6 @@ export default class EmployeesController {
 	async getAllEmployees(req: Request, res: Response): Promise<void> {
 		try {
 			const employees = await Employee.findAll();
-			
 			res.status(200).json({ results: employees.length, data: employees });
 		} catch (err) {
 			res.status(500).json({ err });
@@ -14,6 +13,14 @@ export default class EmployeesController {
 
 	async getEmployee(req: Request, res: Response): Promise<void> {
 		try {
+			const employee = await Employee.findOne({ where: { id: req.params.id } });
+			if (!employee) {
+				res
+					.status(404)
+					.json({ message: `Employee with ${req.params.id} not found.` });
+				return;
+			}
+			res.status(200).json({ data: employee });
 		} catch (err) {
 			res.status(500).json({ err });
 		}
