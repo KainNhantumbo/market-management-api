a4575f19e9f5386ae7b2ec7624f52b9a6f3edb0f
Kain Nhantumbo <nhantumbok@gmail.com>
2022-07-10 20:12:37 +0200

    feat: added delete purchase report controller.

--- src/controllers/purchaseReport.ts
+++ src/controllers/purchaseReport.ts
@@ -37,7 +37,9 @@ export default class PurchaseReportController {
 		try {
 			const new_report = req.body;
 			if (!new_report) {
-				res.status(400).json({ message: 'No data received to create a report.' });
+				res
+					.status(400)
+					.json({ message: 'No data received to create a report.' });
 				return;
 			}
 			await PurchaseReport.create({ ...new_report }, { returning: false });
@@ -46,8 +48,20 @@ export default class PurchaseReportController {
 			res.status(500).json({ err });
 		}
 	}
+  
 	async deletePurchaseReport(req: Request, res: Response) {
 		try {
+			const report_id = Number(req.params.id);
+			if (!report_id) {
+				res
+					.status(400)
+					.json({ message: 'Provided purchase report ID is invalid.' });
+				return;
+			}
+			await PurchaseReport.destroy({ where: { id: report_id } });
+			res
+				.status(200)
+				.json({ message: 'Purchase report data deleted successfuly.' });
 		} catch (err) {
 			res.status(500).json({ err });
 		}
