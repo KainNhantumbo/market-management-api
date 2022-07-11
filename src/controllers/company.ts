1259aa70431f720e974db8d29a2466799a14e70c
Kain Nhantumbo <nhantumbok@gmail.com>
2022-07-11 20:31:28 +0200

    feat: added update company controller.

--- src/controllers/company.ts
+++ src/controllers/company.ts
@@ -13,11 +13,9 @@ export default class CompanyController {
 				where: { id: company_id },
 			});
 			if (!company_details) {
-				res
-					.status(404)
-					.json({
-						message: `Company details data with ID:[${company_id}], was not found.`,
-					});
+				res.status(404).json({
+					message: `Company details data with ID:[${company_id}], was not found.`,
+				});
 				return;
 			}
 			res.status(200).json({ data: company_details });
@@ -28,7 +26,7 @@ export default class CompanyController {
 
 	async createtCompany(req: Request, res: Response): Promise<void> {
 		try {
-      const new_company = req.body;
+			const new_company = req.body;
 			if (!new_company) {
 				res
 					.status(400)
@@ -44,6 +42,24 @@ export default class CompanyController {
 
 	async updateCompany(req: Request, res: Response): Promise<void> {
 		try {
+			const updatedData = req.body;
+			const company_id = Number(req.params.id);
+			if (!company_id) {
+				res.status(400).json({ message: 'Provided company ID is invalid.' });
+				return;
+			} else if (!updatedData) {
+				res
+					.status(400)
+					.json({ message: 'No data received for this update operation.' });
+				return;
+			}
+			await Company.update(
+				{ ...updatedData },
+				{ where: { id: company_id }, returning: false }
+			);
+			res
+				.status(200)
+				.json({ message: 'Company settings updated successfuly.' });
 		} catch (err) {
 			res.status(500).json({ err });
 		}
