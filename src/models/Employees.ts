e6bf975af8c176d8a0c245251c88a5020bf744e3
Kain Nhantumbo <nhantumbok@gmail.com>
2022-07-12 14:36:48 +0200

    feat: added custom validators Employee model.

--- src/models/Employees.ts
+++ src/models/Employees.ts
@@ -7,46 +7,91 @@ Employee.init(
 		first_name: {
 			type: DataTypes.STRING({ length: 50 }),
 			allowNull: false,
+			validate: {
+				notNull: {
+					msg: 'Please enter your first name.',
+				},
+			},
 		},
 		last_name: {
 			type: DataTypes.STRING({ length: 50 }),
 			allowNull: false,
+			validate: {
+				notNull: {
+					msg: 'Please enter your last name.',
+				},
+			},
 		},
 		email: {
 			type: DataTypes.STRING({ length: 50 }),
 			allowNull: false,
 			unique: true,
 			validate: {
-				isEmail: true,
+				isEmail: { msg: 'Please enter a valid e-mail adress.' },
 			},
 		},
 		phone: {
 			type: DataTypes.STRING({ length: 30 }),
 			allowNull: false,
+			validate: {
+				notNull: {
+					msg: 'Please enter your phone number.',
+				},
+			},
 		},
 		adress: {
 			type: DataTypes.STRING({ length: 150 }),
-			allowNull: true,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: 'Please enter your adress.',
+				},
+			},
 		},
 		age: {
 			type: DataTypes.INTEGER,
 			allowNull: false,
+			validate: {
+				notNull: {
+					msg: 'Please enter your age.',
+				},
+			},
 		},
 		gender: {
 			type: DataTypes.STRING({ length: 10 }),
 			allowNull: false,
+			validate: {
+				notNull: {
+					msg: 'Please select your gender.',
+				},
+			},
 		},
 		department: {
 			type: DataTypes.STRING({ length: 250 }),
 			allowNull: false,
+			validate: {
+				notNull: {
+					msg: 'Please enter your department name.',
+				},
+			},
 		},
 		qualification: {
 			type: DataTypes.STRING({ length: 250 }),
 			allowNull: false,
+			validate: {
+				notNull: {
+					msg: 'Please enter your qualification category.',
+				},
+			},
 		},
 		date_of_entry: {
 			type: DataTypes.DATE,
-			allowNull: true,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: 'Please provide the date of entry.',
+				},
+			},
 		},
 		reference_id: {
 			type: DataTypes.UUID,
