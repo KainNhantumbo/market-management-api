e90b241b1a7c23b0fc5a778ca2b29d6c9c4c1dd6
Kain Nhantumbo <nhantumbok@gmail.com>
2022-07-12 14:47:29 +0200

    feat: added custom validators products model.

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -7,34 +7,56 @@ Product.init(
 		name: {
 			type: DataTypes.STRING({ length: 250 }),
 			allowNull: false,
+			validate: {
+				notNull: {
+					msg: 'Please enter product name.',
+				},
+			},
 		},
 		price: {
 			type: DataTypes.FLOAT({ length: 20 }),
 			allowNull: false,
-		},
-		base_price: {
-			type: DataTypes.FLOAT({ length: 20 }),
-			allowNull: false,
+			validate: {
+				notNull: {
+					msg: 'Please enter product price.',
+				},
+			},
 		},
 		type: {
 			type: DataTypes.STRING({ length: 250 }),
 			allowNull: false,
+			validate: {
+				notNull: {
+					msg: 'Please enter product type.',
+				},
+			},
 		},
 		description: {
 			type: DataTypes.STRING({ length: 2500 }),
-			allowNull: true,
+			allowNull: false,
+			validate: {
+				notNull: {
+					msg: 'Please enter product description.',
+				},
+			},
 		},
 		provider: {
 			type: DataTypes.STRING({ length: 250 }),
-			allowNull: true,
+			defaultValue: 'Not specified.'
 		},
 		quantity: {
 			type: DataTypes.INTEGER,
 			allowNull: false,
+			validate: {
+				notNull: {
+					msg: 'Please provide product quantity.',
+				},
+			},
 		},
 		lot: {
 			type: DataTypes.INTEGER,
 			allowNull: true,
+			defaultValue: 0
 		},
 		reference_id: {
 			type: DataTypes.UUID,
