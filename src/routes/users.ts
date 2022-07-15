0e5f0c6cbc65a9fb1e96a95a1cf9dc646847294e
Kain Nhantumbo <nhantumbok@gmail.com>
2022-07-15 23:41:38 +0200

    feat: attached auth middleware to users routes.

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import UserController from '../controllers/users';
+import authenticator from '../middlewares/auth';
 
 const router = Router();
 const controller = new UserController();
 
 router
 	.route('/:id')
-	.get(controller.getUser)
-	.patch(controller.updateUser)
-	.delete(controller.deleteUser);
+	.get(authenticator, controller.getUser)
+	.patch(authenticator, controller.updateUser)
+	.delete(authenticator, controller.deleteUser);
 
 export { router as userRoutes };
