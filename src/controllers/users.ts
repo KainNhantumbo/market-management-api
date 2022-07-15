daa97af100e0244e9efcfc2a1f6534b7d81ff9e6
Kain Nhantumbo <nhantumbok@gmail.com>
2022-07-15 23:05:01 +0200

    performance: improved user controllers code readbility and performance.

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -4,38 +4,31 @@ import { ControllerResponse } from '../types/controller-responses';
 
 export default class UserController {
 	async getUser(req: Request, res: Response): ControllerResponse {
+		const user_id = Number(req.params.id);
+		if (!user_id)
+			return res.status(400).json({ message: 'User ID malformed.' });
 		try {
-			const user_id = Number(req.params.id);
-			if (!user_id) {
-				res.status(400).json({ message: 'Invalid user ID.' });
-				return;
-			}
 			const user = await User.findOne({ where: { id: user_id } });
-			if (!user) {
-				res
+			if (!user)
+				return res
 					.status(404)
 					.json({ message: `User with provided ID [${user_id}] not found.` });
-				return;
-			}
 			res.status(200).json({ data: user });
 		} catch (err) {
 			res.status(500).json({ err });
 		}
 	}
 
-	async updateUser(req: Request, res: Response): ControllerResponse  {
+	async updateUser(req: Request, res: Response): ControllerResponse {
+		const updatedData = req.body;
+		const user_id = Number(req.params.id);
+		if (!user_id)
+			return res.status(400).json({ message: 'User ID malformed.' });
+		if (!updatedData)
+			return res
+				.status(400)
+				.json({ message: 'No data received for this update operation.' });
 		try {
-			const updatedData = req.body;
-			const user_id = Number(req.params.id);
-			if (!user_id) {
-				res.status(400).json({ message: 'Provided user ID is invalid.' });
-				return;
-			} else if (!updatedData) {
-				res
-					.status(400)
-					.json({ message: 'No data received for this update operation.' });
-				return;
-			}
 			await User.update(
 				{ ...updatedData },
 				{ where: { id: user_id }, returning: false }
@@ -46,13 +39,11 @@ export default class UserController {
 		}
 	}
 
-	async deleteUser(req: Request, res: Response): ControllerResponse  {
+	async deleteUser(req: Request, res: Response): ControllerResponse {
+		const user_id = Number(req.params.id);
+		if (!user_id)
+			return res.status(400).json({ message: 'User ID malformed.' });
 		try {
-			const user_id = Number(req.params.id);
-			if (!user_id) {
-				res.status(400).json({ message: 'Provided user ID is invalid.' });
-				return;
-			}
 			await User.destroy({ where: { id: user_id } });
 			res.status(200).json({ message: 'User data deleted successfuly.' });
 		} catch (err) {
