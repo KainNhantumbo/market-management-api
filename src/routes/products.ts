260ae62d1dda60851f131864bdccb60ab7f5c4c5
Kain Nhantumbo <nhantumbok@gmail.com>
2022-07-15 23:39:37 +0200

    feat: attached auth middleware to products routes.

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/products';
+import authenticator from '../middlewares/auth';
 
 const controller = new ProductsController();
 const router = Router();
 
-router.route('/').get(controller.getProducts).post(controller.createProduct);
+router
+	.route('/')
+	.get(authenticator, controller.getProducts)
+	.post(authenticator, controller.createProduct);
 
 router
 	.route('/:id')
-	.get(controller.getProduct)
-	.patch(controller.updateProduct)
-	.delete(controller.deleteProduct);
+	.get(authenticator, controller.getProduct)
+	.patch(authenticator, controller.updateProduct)
+	.delete(authenticator, controller.deleteProduct);
 
 export { router as productsRoutes };
