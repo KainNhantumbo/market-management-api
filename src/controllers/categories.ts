4e639c9251c1820fbf8ca9f094e7e85aa5642335
Kain Nhantumbo <nhantumbok@gmail.com>
2022-07-18 21:20:34 +0200

    refactor: proper error handling in category routes and controllers.

--- src/controllers/categories.ts
+++ src/controllers/categories.ts
@@ -1,93 +1,65 @@
 import Category from '../models/Category';
 import { Request, Response } from 'express';
 import { ControllerResponse } from '../types/controller-responses';
+import BaseError from '../errors/base-error';
 
 export default class CategoriesController {
 	async getCategories(req: Request, res: Response): ControllerResponse {
 		const user_ref = (req as any).user.ref;
-		try {
-			const categories = await Category.findAll({
-				where: { createdBy: user_ref },
-			});
-			res.status(200).json({ results: categories.length, data: categories });
-		} catch (err: any) {
-			res.status(500).json({ err });
-		}
+		const categories = await Category.findAll({
+			where: { createdBy: user_ref },
+		});
+		res.status(200).json({ results: categories.length, data: categories });
 	}
 
 	async getCategory(req: Request, res: Response): ControllerResponse {
-		const category_id = Number(req.params.id);
+		const category_id = Number.parseInt(req.params.id);
 		const user_ref = (req as any).user.ref;
 		if (!category_id)
-			return res
-				.status(400)
-				.json({ message: 'Provided category ID is invalid.' });
-		try {
-			const category = await Category.findOne({
-				where: { id: category_id, createdBy: user_ref },
-			});
-			if (!category)
-				return res.status(404).json({
-					message: `No category with ID:[${category_id}] was found.`,
-				});
-			res.status(200).json({ data: category });
-		} catch (err) {
-			res.status(500).json({ err });
-		}
+			throw new BaseError('Provided category ID is invalid.', 400);
+		const category = await Category.findOne({
+			where: { id: category_id, createdBy: user_ref },
+		});
+		if (!category)
+			throw new BaseError(
+				`No category with ID:[${category_id}] was found.`,
+				404
+			);
+		res.status(200).json({ data: category });
 	}
 
 	async createCategory(req: Request, res: Response): ControllerResponse {
 		const new_category = req.body;
 		new_category.createdBy = (req as any).user.ref;
 		if (!new_category)
-			return res
-				.status(400)
-				.json({ message: 'No data received to save category.' });
-		try {
-			await Category.create({ ...new_category }, { returning: false });
-			res.status(201).json({ message: 'Category saved successfuly.' });
-		} catch (err: any) {
-			res.status(500).json({ err });
-		}
+			throw new BaseError('No data received to save category.', 400);
+		await Category.create({ ...new_category }, { returning: false });
+		res.status(201).json({ message: 'Category saved successfuly.' });
 	}
 
 	async updateCategory(req: Request, res: Response): ControllerResponse {
 		const updatedData = req.body;
-		const category_id = Number(req.params.id);
+		const category_id = Number.parseInt(req.params.id);
 		const user_ref = (req as any).user.ref;
 		if (!category_id)
-			return res
-				.status(400)
-				.json({ message: 'Provided category ID is invalid.' });
+			throw new BaseError('Provided category ID is invalid.', 400);
 		if (!updatedData)
-			return res
-				.status(400)
-				.json({ message: 'No data received for update operation.' });
-		try {
-			await Category.update(
-				{ ...updatedData },
-				{ where: { id: category_id, createdBy: user_ref }, returning: false }
-			);
-			res.status(200).json({ message: 'Category updated successfuly.' });
-		} catch (err) {
-			res.status(500).json({ err });
-		}
+			throw new BaseError('No data received for update operation.', 400);
+		await Category.update(
+			{ ...updatedData },
+			{ where: { id: category_id, createdBy: user_ref }, returning: false }
+		);
+		res.status(200).json({ message: 'Category updated successfuly.' });
 	}
 
 	async deleteCategory(req: Request, res: Response): ControllerResponse {
-		const category_id = Number(req.params.id);
+		const category_id = Number.parseInt(req.params.id);
 		const user_ref = (req as any).user.ref;
 		if (!category_id)
-			return res
-				.status(400)
-				.json({ message: 'Provided category ID is invalid.' });
-		try {
-			await Category.destroy({
-				where: { id: category_id, createdBy: user_ref },
-			});
-			res.status(200).json({ message: 'Category deleted successfuly.' });
-		} catch (err) {
-			res.status(500).json({ err });
-		}
+			throw new BaseError('Provided category ID is invalid.', 400);
+		await Category.destroy({
+			where: { id: category_id, createdBy: user_ref },
+		});
+		res.status(200).json({ message: 'Category deleted successfuly.' });
 	}
 }
--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express';
 import CategoriesController from '../controllers/categories';
+import use from '../utils/async-wrapper';
 
 const router = Router();
 const controller = new CategoriesController();
 
-router.route('/').get(controller.getCategories).post(controller.createCategory);
+router
+	.route('/')
+	.get(use(controller.getCategories))
+	.post(use(controller.createCategory));
 router
 	.route('/:id')
-	.get(controller.getCategory)
-	.patch(controller.updateCategory)
-	.delete(controller.deleteCategory);
+	.get(use(controller.getCategory))
+	.patch(use(controller.updateCategory))
+	.delete(use(controller.deleteCategory));
 
 export { router as categoriesRoutes };
